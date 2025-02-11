41d58a176ee52ba90c49e3fa25f2e30226938c8c
Mark Zhurbenko <mlol6519@gmail.com>
2025-02-12 00:00:22 +0300

    добавил карту и вступительный текст

--- website/APP/views.py
+++ website/APP/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html') #http://127.0.0.1:8000/home/
